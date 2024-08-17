e6b3cbdba869704924ff02d849f0e674e8892144
ravi7501 <ravisingh7501@gmail.com>
2024-08-17 15:08:13 +0530

    reverse for position

--- linklist/linklist_algo_question.py
+++ linklist/linklist_algo_question.py
@@ -7,7 +7,16 @@ from linklistfirst import  node,single_linklist
 def linklist_reverse(head):
     curr = head 
     prev = None
-    
+    while curr is not None:
+        next_node=curr.next 
+        curr.next = prev
+        prev = curr
+        curr = next_node
+    return prev
+
+
+
+
     print(head)
 
 
@@ -18,4 +27,6 @@ if __name__=="__main__":
     li.insert_from_front(89)
     li.insert_from_back(34)
     li.insert_from_back(12)
+    li.display_linklist()
+    li.head=linklist_reverse(li.head)
     li.display_linklist()
